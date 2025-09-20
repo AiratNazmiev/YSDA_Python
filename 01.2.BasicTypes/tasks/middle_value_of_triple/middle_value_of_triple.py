1edb680e504bf60efa36ec554fcebf793dc55f70
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-09-20 03:57:28 +0300

    middle_value_of_triple

--- 01.2.BasicTypes/tasks/middle_value_of_triple/middle_value_of_triple.py
+++ 01.2.BasicTypes/tasks/middle_value_of_triple/middle_value_of_triple.py
@@ -2,3 +2,9 @@ def get_middle_value(a: int, b: int, c: int) -> int:
     """
     Takes three values and returns middle value.
     """
+    if (a >= b or c >= b) and (b >= c or b >= a):
+        return b
+    elif (a >= c or b >= c) and (c >= b or c >= a):
+        return c
+    else:
+        return a
