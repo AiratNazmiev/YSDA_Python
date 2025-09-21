83aabbba15e9dde950b28b2603149c74fe4adc71
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-09-21 22:31:54 +0300

    filter_list_by_list

--- 01.2.BasicTypes/tasks/filter_list_by_list/filter_list_by_list.py
+++ 01.2.BasicTypes/tasks/filter_list_by_list/filter_list_by_list.py
@@ -5,3 +5,20 @@ def filter_list_by_list(lst_a: list[int] | range, lst_b: list[int] | range) -> l
     :param lst_b: second sorted list
     :return: filtered sorted list
     """
+    idx_a = 0
+    idx_b = 0
+    result = []
+
+    while idx_a != len(lst_a) and idx_b != len(lst_b):
+        if lst_a[idx_a] > lst_b[idx_b]:
+            idx_b += 1
+        elif lst_a[idx_a] == lst_b[idx_b]:
+            idx_a += 1
+        else:
+            result.append(lst_a[idx_a])
+            idx_a += 1
+
+    if idx_a != len(lst_a):
+        result.extend(lst_a[idx_a:])
+
+    return result
