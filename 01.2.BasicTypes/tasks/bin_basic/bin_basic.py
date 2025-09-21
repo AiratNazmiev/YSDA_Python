2e6bd20d56aa5e41945f26988abaa4460af9f3b2
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-09-21 22:48:23 +0300

    bin_basic

--- 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
@@ -5,3 +5,15 @@ def find_value(nums: list[int] | range, value: int) -> bool:
     :param value: integer to find
     :return: True if value exists, False otherwise
     """
+    left = 0
+    right = len(nums)
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < value:
+            left = mid + 1
+        elif nums[mid] > value:
+            right = mid
+        else:
+            return True
+
+    return False
