a27e7cfbcc0484ca7a844b9d66c6987923cf77f5
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-03 22:16:00 +0300

    minor upd

--- 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
+++ 01.2.BasicTypes/tasks/bin_basic/bin_basic.py
@@ -1,3 +1,22 @@
+# def find_value(nums: list[int] | range, value: int) -> bool:
+#     """
+#     Find value in sorted sequence
+#     :param nums: sequence of integers. Could be empty
+#     :param value: integer to find
+#     :return: True if value exists, False otherwise
+#     """
+#     left = 0
+#     right = len(nums)
+#     while left < right:
+#         mid = (left + right) // 2
+#         if nums[mid] < value:
+#             left = mid + 1
+#         elif nums[mid] > value:
+#             right = mid
+#         else:
+#             return True
+
+#     return False
 def find_value(nums: list[int] | range, value: int) -> bool:
     """
     Find value in sorted sequence
@@ -6,13 +25,13 @@ def find_value(nums: list[int] | range, value: int) -> bool:
     :return: True if value exists, False otherwise
     """
     left = 0
-    right = len(nums)
-    while left < right:
+    right = len(nums) - 1
+    while left <= right:
         mid = (left + right) // 2
         if nums[mid] < value:
             left = mid + 1
         elif nums[mid] > value:
-            right = mid
+            right = mid - 1
         else:
             return True
 
