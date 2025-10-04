b956126eb64c39534631df7c9809bc25e6e4c8bb
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-04 03:19:04 +0300

    alphabet - topo sort

--- 02.1.DataStructures_hard/tasks/alphabet/alphabet.py
+++ 02.1.DataStructures_hard/tasks/alphabet/alphabet.py
@@ -15,6 +15,33 @@ def extract_alphabet(
     :param graph: graph with partial order
     :return: alphabet
     """
+    alphabet: list[str] = []
+    status: dict[str, Status] = {v: Status.NEW for v in graph}
+
+    for v in graph:
+        if status[v] == Status.FINISHED:
+            continue
+
+        stack: list[str] = [v]
+
+        while stack:
+            to = stack.pop()
+
+            if status[to] == Status.NEW:
+                status[to] = Status.EXTRACTED
+                stack.append(to)
+                for ch in graph[to]:
+                    if status[ch] == Status.NEW:
+                        stack.append(ch)
+                    elif status[ch] == Status.EXTRACTED:
+                        raise ValueError("There is a cycle in graph")
+            elif status[to] == Status.EXTRACTED:
+                status[to] = Status.FINISHED
+                alphabet.append(to)
+
+    alphabet.reverse()
+
+    return alphabet
 
 
 def build_graph(
@@ -25,6 +52,20 @@ def build_graph(
     :param words: ordered words
     :return: graph
     """
+    graph: dict[str, set[str]] = {}
+
+    for word in words:
+        for char in word:
+            if char not in graph:
+                graph[char] = set()
+
+    for w1, w2 in zip(words[:-1], words[1:]):
+        for c1, c2 in zip(w1, w2):
+            if c1 != c2:
+                graph[c1].add(c2)
+                break
+
+    return graph
 
 
 #########################
