1b0be5b933d67a12d33170d4905ce9c0732f160f
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-16 00:45:03 +0300

    caesar_cipher

--- 03.1.FunctionsStringsIO/tasks/caesar_cipher/caesar_cipher.py
+++ 03.1.FunctionsStringsIO/tasks/caesar_cipher/caesar_cipher.py
@@ -1,3 +1,10 @@
+def caesar_encrypt_char(c: str, n: int) -> str:
+    if not c.isalpha():
+        return c
+    start = ord('a' if c.islower() else 'A')
+    return  chr(start + (ord(c) - start + n) % 26)
+
+
 def caesar_encrypt(message: str, n: int) -> str:
     """Encrypt message using caesar cipher
 
@@ -5,3 +12,4 @@ def caesar_encrypt(message: str, n: int) -> str:
     :param n: shift
     :return: encrypted message
     """
+    return ''.join(map(lambda c: caesar_encrypt_char(c, n=n), message))
