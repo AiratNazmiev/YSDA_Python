b5e8de7bb7d5f3c0893ed920a5a4fa0215ccf175
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-16 01:55:32 +0300

    merge_lists_3 - byte streams

--- 03.1.FunctionsStringsIO/tasks/merge_lists_3/merge_lists_3.py
+++ 03.1.FunctionsStringsIO/tasks/merge_lists_3/merge_lists_3.py
@@ -1,3 +1,4 @@
+import heapq
 import typing as tp
 
 
@@ -8,3 +9,12 @@ def merge(input_streams: tp.Sequence[tp.IO[bytes]], output_stream: tp.IO[bytes])
     :param output_stream: output stream. Contains byte-strings separated by "\n". Nonempty stream ends with "\n"
     :return: None
     """
+    min_heap = [(int(line), seq_idx) for seq_idx, s in enumerate(input_streams) if (line := s.readline())]
+    heapq.heapify(min_heap)
+
+    while min_heap:
+        curr_min, curr_seq_idx = heapq.heappop(min_heap)
+        output_stream.write(f"{curr_min}\n".encode())
+        next_elem = input_streams[curr_seq_idx].readline()
+        if next_elem:
+            heapq.heappush(min_heap, (int(next_elem), curr_seq_idx))
