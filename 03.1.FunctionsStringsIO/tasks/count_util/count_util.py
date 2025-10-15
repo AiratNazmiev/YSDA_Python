2bfeb45f9221fec44013fc09647188f210aa28c9
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-16 01:08:29 +0300

    count_util

--- 03.1.FunctionsStringsIO/tasks/count_util/count_util.py
+++ 03.1.FunctionsStringsIO/tasks/count_util/count_util.py
@@ -1,3 +1,4 @@
+import re
 
 
 def count_util(text: str, flags: str | None = None) -> dict[str, int]:
@@ -19,3 +20,16 @@ def count_util(text: str, flags: str | None = None) -> dict[str, int]:
         * "longest_line" - the longest line length
         * "words" - amount of words
     """
+    flags = "-mlLw" if not flags else flags
+    flags_list = [c for s in re.findall(r"-(\w+)", flags) for c in s]
+    result = {}
+    if 'm' in flags_list:
+        result["chars"] = len(text)
+    if 'l' in flags_list:
+        result["lines"] = text.count('\n')
+    if 'L' in flags_list:
+        result["longest_line"] = max(map(len, text.split('\n')))
+    if 'w' in flags_list:
+        result["words"] = len(text.split())
+
+    return result
