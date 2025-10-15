762b7e40136a2c21fdd1f1bf67d32f1300694009
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-16 02:38:11 +0300

    normalize path

--- 03.1.FunctionsStringsIO/tasks/normalize_path/normalize_path.py
+++ 03.1.FunctionsStringsIO/tasks/normalize_path/normalize_path.py
@@ -3,3 +3,24 @@ def normalize_path(path: str) -> str:
     :param path: unix path to normalize
     :return: normalized path
     """
+    is_abs = path.startswith('/')
+
+    parts = path.split('/')
+    stack = []
+
+    for p in parts:
+        if p == '' or p == '.':
+            continue
+        if p == '..':
+            if stack and stack[-1] != '..':
+                stack.pop()
+            else:
+                if not is_abs:
+                    stack.append('..')
+        else:
+            stack.append(p)
+
+    if is_abs:
+        return '/' + '/'.join(stack)
+    else:
+        return '/'.join(stack) if stack else '.'
