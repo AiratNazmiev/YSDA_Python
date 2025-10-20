f0d1a2a5e1d4a8cd31e3dbcba1c37b48db646be5
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-20 03:42:59 +0300

    codeops

--- 04.2.Bytecode/tasks/codeops/codeops.py
+++ 04.2.Bytecode/tasks/codeops/codeops.py
@@ -1,4 +1,6 @@
 import types
+import dis
+from collections import Counter
 
 
 def count_operations(source_code: types.CodeType) -> dict[str, int]:
@@ -7,3 +9,13 @@ def count_operations(source_code: types.CodeType) -> dict[str, int]:
     :param source_code: the bytecode operation names to be extracted from
     :return: operation counts
     """
+    ops_cnt: Counter = Counter()
+    instructions = dis.get_instructions(source_code)
+    for cur_instruction in instructions:
+        ops_cnt[cur_instruction.opname] += 1
+
+        if isinstance(cur_instruction.argval, types.CodeType):
+            inner_ops_cnt = count_operations(cur_instruction.argval)
+            ops_cnt += inner_ops_cnt
+
+    return ops_cnt
