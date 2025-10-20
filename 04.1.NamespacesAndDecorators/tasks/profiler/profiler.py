938d0b4bde7856017aa2ca69936e3e57a1275cb3
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-20 05:31:06 +0300

    profiler

--- 04.1.NamespacesAndDecorators/tasks/profiler/profiler.py
+++ 04.1.NamespacesAndDecorators/tasks/profiler/profiler.py
@@ -1,3 +1,7 @@
+from functools import wraps
+from datetime import datetime
+
+
 def profiler(func):  # type: ignore
     """
     Returns profiling decorator, which counts calls of function
@@ -6,3 +10,23 @@ def profiler(func):  # type: ignore
     :param func: function to decorate
     :return: decorator, which wraps any function passed
     """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if wrapper._depth == 0:
+            wrapper.calls = 1
+        else:
+            wrapper.calls += 1
+        wrapper._depth += 1
+        start_time = datetime.now()
+        result = func(*args, **kwargs)
+        end_time = datetime.now()
+        wrapper.last_time_taken = (end_time - start_time).total_seconds()
+        wrapper._depth -= 1
+        return result
+
+    wrapper.calls = 0
+    wrapper.last_time_taken = 0.
+    wrapper._depth = 0
+
+    return wrapper
