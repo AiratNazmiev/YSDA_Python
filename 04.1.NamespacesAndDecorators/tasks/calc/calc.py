aead68d361504965d62b1548454a2b3a4107c732
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-20 05:09:03 +0300

    calc - eval

--- 04.1.NamespacesAndDecorators/tasks/calc/calc.py
+++ 04.1.NamespacesAndDecorators/tasks/calc/calc.py
@@ -7,6 +7,16 @@ PROMPT = '>>> '
 
 def run_calc(context: dict[str, Any] | None = None) -> None:
     """Run interactive calculator session in specified namespace"""
+    context = {'__builtins__': {}} if context is None else context
+    while True:
+        sys.stdout.write(PROMPT)
+        data = sys.stdin.readline()
+        if data:
+            sys.stdout.write(f"{str(eval(data, context))}\n")
+        else:
+            break
+
+    sys.stdout.write('\n')
 
 
 if __name__ == '__main__':
