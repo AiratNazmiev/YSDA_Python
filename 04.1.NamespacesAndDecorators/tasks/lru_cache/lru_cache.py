c419a4c2315712548c6506abf76d796769f1d119
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-10-22 03:34:23 +0300

    lru_cache - interesting typings (wraps, cast)

--- 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
+++ 04.1.NamespacesAndDecorators/tasks/lru_cache/lru_cache.py
@@ -1,14 +1,44 @@
 from collections.abc import Callable
-from typing import Any, TypeVar
+from collections import OrderedDict
+import functools
+import typing as tp
 
 
-Function = TypeVar('Function', bound=Callable[..., Any])
+Function = tp.TypeVar("Function", bound=tp.Callable[..., tp.Any])
 
 
-def cache(max_size: int) -> Callable[[Function], Function]:
+def cache(max_size: int) -> tp.Callable[[Function], Function]:
     """
     Returns decorator, which stores result of function
     for `max_size` most recent function arguments.
     :param max_size: max amount of unique arguments to store values for
     :return: decorator, which wraps any function passed
     """
+
+    def _make_key(args, kwargs: dict | None = None):
+        if kwargs:
+            return (args, sorted((k, v) for k, v in kwargs.items()))
+
+        return args
+
+    def wraps(func: Function) -> Function:
+        cached_results_dict: OrderedDict[tp.Any, tp.Any] = OrderedDict()
+
+        @functools.wraps(func)
+        def wrapper(*args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+            key = _make_key(args, kwargs)
+            if key in cached_results_dict:
+                cached_results_dict.move_to_end(key, last=True)
+                return cached_results_dict[key]
+
+            result = func(*args, **kwargs)
+            cached_results_dict[key] = result
+
+            if len(cached_results_dict) > max_size:
+                cached_results_dict.popitem(last=False)
+
+            return result
+
+        return tp.cast(Function, wrapper)
+
+    return wraps
