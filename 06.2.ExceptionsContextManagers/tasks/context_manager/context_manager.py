bb9907fd94938e382b7e615b3a5b698252095cc0
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-04 04:50:54 +0300

    @contextmanager

--- 06.2.ExceptionsContextManagers/tasks/context_manager/context_manager.py
+++ 06.2.ExceptionsContextManagers/tasks/context_manager/context_manager.py
@@ -1,17 +1,42 @@
 from contextlib import contextmanager
 from typing import Iterator, TextIO, Type
+import sys
+import traceback
 
 
 @contextmanager
 def supresser(*types_: Type[BaseException]) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    except Exception as exc:
+        type_exc, value_exc, trace_exc = sys.exc_info()
+        if type_exc not in types_:
+            raise exc
 
 
 @contextmanager
 def retyper(type_from: Type[BaseException], type_to: Type[BaseException]) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    except Exception as exc:
+        type_exc, value_exc, trace_exc = sys.exc_info()
+        if type_from == type_exc:
+            if value_exc is not None:
+                raise type_to(*value_exc.args)
+            else:
+                raise type_to
+        else:
+            raise exc
 
 
 @contextmanager
 def dumper(stream: TextIO | None = None) -> Iterator[None]:
-    pass
+    try:
+        yield None
+    except Exception:
+        type_exc, value_exc, trace_exc = sys.exc_info()
+        if stream is None:
+            sys.stderr.write(traceback.format_exception_only(type_exc, value_exc)[0])
+        else:
+            stream.write(traceback.format_exception_only(type_exc, value_exc)[0])
+        raise
