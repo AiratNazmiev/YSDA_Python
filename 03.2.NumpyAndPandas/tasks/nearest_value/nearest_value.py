b166616ea6f729ffa76a25f0c53cf51b3297d0ea
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-04 16:30:07 +0300

    nearest value, .flat

--- 03.2.NumpyAndPandas/tasks/nearest_value/nearest_value.py
+++ 03.2.NumpyAndPandas/tasks/nearest_value/nearest_value.py
@@ -10,3 +10,4 @@ def nearest_value(matrix: npt.NDArray[np.float64], value: float) -> float | None
     :param value: value to find
     :return: nearest value in matrix or None
     """
+    return float(matrix.flat[np.argmin(np.abs(matrix - value))]) if matrix.size != 0 else None
