d1541c334e53b83fb9af918b2f897d18249235a0
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-13 17:49:49 +0300

    flat_it

--- 07.1.IteratorsGenerators/tasks/flat_it/flat_it.py
+++ 07.1.IteratorsGenerators/tasks/flat_it/flat_it.py
@@ -7,3 +7,8 @@ def flat_it(sequence: Iterable[Any]) -> Iterator[Any]:
     :param sequence: iterable with arbitrary level of nested iterables
     :return: generator producing flatten sequence
     """
+    for item in sequence:
+        if isinstance(item, Iterable) and not (isinstance(item, str) and len(item) == 1):
+            yield from flat_it(item)
+        else:
+            yield item
