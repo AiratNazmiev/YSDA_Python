8c913c0060e478f0fcd3802fc2b8f15815cb9197
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-26 23:29:03 +0300

    typy protocol

--- 08.1.Typing_2/tasks/typy_protocol/typy_protocol.py
+++ 08.1.Typing_2/tasks/typy_protocol/typy_protocol.py
@@ -1,10 +1,17 @@
+from typing import Protocol, TypeVar, Optional
+
+T_co = TypeVar("T_co", covariant=True)
 
 
-class Gettable:
-    pass
+class Gettable(Protocol[T_co]):
+    def __getitem__(self, index: int) -> T_co:
+        ...
+
+    def __len__(self) -> int:
+        ...
 
 
-def get(container, index):
+def get(container: Gettable[T_co], index: int) -> Optional[T_co]:
     if container:
         return container[index]
 
