fd43a9faeecfeb80a9416cfb7e55c2fa3d75b97f
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-26 23:34:52 +0300

    fix

--- 08.1.Typing_2/tasks/typy_generic/typy_generic.py
+++ 08.1.Typing_2/tasks/typy_generic/typy_generic.py
@@ -1,12 +1,27 @@
+from typing import Generic, TypeVar, cast
+
+T = TypeVar("T", int, float)
 
 
-class Pair:
-    def __init__(self, a, b): pass
+class Pair(Generic[T]):
+    def __init__(self, a: T, b: T) -> None:
+        self._a: T = a
+        self._b: T = b
 
-    def sum(self): pass
+    def sum(self) -> T:
+        tmp_sum = cast(int | float, self._a) + cast(int | float, self._b)
+        return cast(T, tmp_sum)
 
-    def first(self): pass
+    def first(self) -> T:
+        return self._a
 
-    def second(self): pass
+    def second(self) -> T:
+        return self._b
 
-    def __iadd__(self, pair): pass
+    def __iadd__(self, pair: "Pair[T]") -> "Pair[T]":
+        new_a = cast(int | float, self._a) + cast(int | float, pair._a)
+        new_b = cast(int | float, self._b) + cast(int | float, pair._b)
+
+        self._a = cast(T, new_a)
+        self._b = cast(T, new_b)
+        return self
