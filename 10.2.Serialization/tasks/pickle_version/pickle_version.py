c41b34c9caef8a760c69b22345292afc1cd0c3ab
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-11-30 18:59:29 +0300

    pickle version

--- 10.2.Serialization/tasks/pickle_version/pickle_version.py
+++ 10.2.Serialization/tasks/pickle_version/pickle_version.py
@@ -1,10 +1,11 @@
 import dataclasses
+import pickletools
 
 
 @dataclasses.dataclass
 class PickleVersion:
-	is_new_format: bool
-	version: int
+    is_new_format: bool
+    version: int
 
 
 def get_pickle_version(data: bytes) -> PickleVersion:
@@ -14,3 +15,12 @@ def get_pickle_version(data: bytes) -> PickleVersion:
     :param data: serialized object in pickle format.
     :return: protocol version.
     """
+    version = next(pickletools.genops(data))[1]
+    if version is None or version < 2:
+        version = -1
+        is_new_format = False
+    else:
+        version = version
+        is_new_format = True
+
+    return PickleVersion(is_new_format, version)
