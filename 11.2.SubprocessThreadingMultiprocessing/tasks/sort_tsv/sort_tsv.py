22cd9f411a666858a8d08b0ff1044780a4cf7b44
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-12-12 18:39:35 +0300

    sort tsv

--- 11.2.SubprocessThreadingMultiprocessing/tasks/sort_tsv/sort_tsv.py
+++ 11.2.SubprocessThreadingMultiprocessing/tasks/sort_tsv/sort_tsv.py
@@ -8,14 +8,15 @@ def python_sort(file_in: Path, file_out: Path) -> None:
     :param file_in: tsv file to read from
     :param file_out: tsv file to write to
     """
-    lines = map(lambda x: x.split(), file_in.read_text(encoding="utf-8").splitlines())
-    lines_sorted = sorted(
-        lines,
-        key=lambda x: (int(x[1]), x[0])
-    )
+    lines = file_in.read_text(encoding="utf-8").splitlines()
 
-    with open(file_out, 'w') as f:
-        f.write(''.join([f"{row[0]}\t{row[1]}\n" for row in lines_sorted]) + '\n')
+    def key_func(line: str) -> tuple[int, str]:
+        first_col, second_col = line.split("\t", 1)
+        return int(second_col), first_col
+
+    lines.sort(key=key_func)
+
+    file_out.write_text("".join(f"{line}\n" for line in lines))
 
 
 def util_sort(file_in: Path, file_out: Path) -> None:
@@ -24,14 +25,7 @@ def util_sort(file_in: Path, file_out: Path) -> None:
     :param file_in: tsv file to read from
     :param file_out: tsv file to write to
     """
-    cmd = [
-        "sort",
-        "-t", "\t",
-        "-k2,2n",
-        "-k1,1",
-        "--stable",
-        str(file_in),
-        "-o", str(file_out),
-    ]
+    cmd = ["sort", "-t", "\t", "-k2,2n", "-k1,1", str(file_in)]
 
-    subprocess.run(cmd, check=True)
+    with file_out.open("w", newline="") as fout:
+        subprocess.run(cmd, stdout=fout, check=True)
