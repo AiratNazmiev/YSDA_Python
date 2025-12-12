5191415c382060c39774b3a136e8f64ccb7c284c
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-12-12 19:07:15 +0300

    very slow function - threading, multiprocessing

--- 11.2.SubprocessThreadingMultiprocessing/tasks/very_slow_function/very_slow_function.py
+++ 11.2.SubprocessThreadingMultiprocessing/tasks/very_slow_function/very_slow_function.py
@@ -1,4 +1,7 @@
 import time
+import threading
+import multiprocessing
+import os
 
 
 def very_slow_function(x: int) -> int:
@@ -15,7 +18,7 @@ def calc_squares_simple(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
-
+    return [very_slow_function(x) for x in range(bound)]
 
 def calc_squares_multithreading(bound: int) -> list[int]:
     """Function that calculates squares of numbers in range [0; bound)
@@ -23,7 +26,21 @@ def calc_squares_multithreading(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
+    results = [0] * bound
 
+    def worker(x: int) -> None:
+        results[x] = very_slow_function(x)
+
+    threads = []
+    for x in range(bound):
+        t = threading.Thread(target=worker, args=(x, ))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return results
 
 def calc_squares_multiprocessing(bound: int) -> list[int]:
     """Function that calculates squares of numbers in range [0; bound)
@@ -31,3 +48,7 @@ def calc_squares_multiprocessing(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
+
+    workers = min(bound, os.cpu_count() or 1)
+    with multiprocessing.Pool(processes=workers) as pool:
+        return pool.map(very_slow_function, range(bound))
