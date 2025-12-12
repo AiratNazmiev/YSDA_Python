8fd31c746669291374819836fa0df4bd66b5e857
AiratNazmiev <nazmiev.airat@yandex.ru>
2025-12-12 17:04:35 +0300

    git_diff

--- 11.2.SubprocessThreadingMultiprocessing/tasks/git_diff/git_diff.py
+++ 11.2.SubprocessThreadingMultiprocessing/tasks/git_diff/git_diff.py
@@ -10,3 +10,11 @@ def get_changed_dirs(git_path: Path, from_commit_hash: str, to_commit_hash: str)
     :param to_commit_hash: hash of commit to do diff to
     :return: sequence of changed directories between specified commits
     """
+    output = subprocess.run(
+        ["git", "diff", from_commit_hash, to_commit_hash, "--dirstat"],
+        cwd=git_path,
+        capture_output=True
+    )
+    changed_dirs = set(map(lambda x: git_path/Path(x.strip('/')), output.stdout.decode().split()[1::2]))
+
+    return changed_dirs
